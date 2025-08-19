a9514119b38351c218416bde24758cec7267e52d
Rishit <rishupal631@gmail.com>
2025-08-19 14:14:12 +0000

    ellipse init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,30 @@
 "use client"
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-interface Shape{
-  "startingX":number,
-  "startingY":number,
-  "width":number,
-  "height":number
+interface Rectangle {
+  type: "rectangle";
+  startingX: number;
+  startingY: number;
+  width: number;
+  height: number;
 }
+
+interface Ellipse {
+  type: "ellipse";
+  centerX: number;
+  centerY: number;
+  radiusX: number;
+  radiusY: number;
+}
+
+type Shape= Rectangle | Ellipse
 
 export default function Home() {
   const canvasRef= useRef<HTMLCanvasElement>(null);
   const shapesRef = useRef<Shape[]>([]);
+  const [selected,setSelected]=useState('circle')
+
+  
   useEffect(()=>{
 
     const canvas= canvasRef.current;
@@ -22,44 +36,127 @@ export default function Home() {
     const ctx= canvas.getContext('2d');
     if (!ctx) return;
     
-    for(const shape of shapesRef.current){
-      ctx.strokeRect(shape.startingX, shape.startingY, shape.width, shape.height);
-    }
-
     ctx.strokeStyle="white"
-    let stX=0,stY=0
-    let clicked=false
 
-    canvas.addEventListener('mousedown',(e)=>{
-      clicked=true
-      stX=e.clientX
-      stY=e.clientY
-      console.log(e.clientX);
-      console.log(e.clientY)
-    })
-    
-    canvas.addEventListener('mouseup',(e)=>{
-      clicked=false
-      shapesRef.current.push({startingX:stX,startingY:stY,width:e.clientX, height:e.clientY})
-    })
-    
-    canvas.addEventListener('mousemove',(e)=>{
-      if(clicked){
-        const h=e.clientY-stY;
-        const w=e.clientX-stX
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeRect(stX,stY,w,h);
+    const renderShapes=()=>{
+      ctx.clearRect(0,0,canvas.width,canvas.height)
+        for(const shape of shapesRef.current){
+          if(shape.type=="rectangle"){
+            ctx.strokeRect(shape.startingX, shape.startingY, shape.width, shape.height);
+          }
+          else if(shape.type=="ellipse"){
+            ctx.beginPath()
+            ctx.ellipse(shape.centerX, shape.centerY, shape.radiusX, shape.radiusY,0,0,2*Math.PI);
+            ctx.stroke()
+          }
+          
+        }
+    }
+    renderShapes()
+   if(selected=='rectangle'){
+      let stX=0,stY=0
+      let clicked=false;
+
+      const handleMousedown=(e:MouseEvent)=>{
+            clicked=true
+            stX=e.clientX
+            stY=e.clientY
+      }
+      const handleMouseup=(e:MouseEvent)=>{
+            clicked=false
+            shapesRef.current.push({type:"rectangle",startingX:stX,startingY:stY,width:e.clientX-stX, height:e.clientY-stY})
+      }
+      const handleMousemove=(e:MouseEvent)=>{
+        if(clicked){
+          renderShapes()       
+          const h=e.clientY-stY;
+          const w=e.clientX-stX
+          ctx.strokeRect(stX,stY,w,h);
+        }
       }
 
-    })
+      canvas.addEventListener('mousedown',handleMousedown)
+      canvas.addEventListener('mouseup',handleMouseup)
+      canvas.addEventListener('mousemove',handleMousemove)
+      return ()=>{
+        canvas.removeEventListener("mousedown", handleMousedown);
+        canvas.removeEventListener("mouseup", handleMouseup);
+        canvas.removeEventListener("mousemove", handleMousemove);
+      }
+   }
 
-  },[canvasRef])
+   else if(selected=='ellipse'){
+   
+    let stX=0,stY=0;
+    let clicked=false;
+    const handleMousedown=(e:MouseEvent)=>{
+      clicked=true;
+       stX=e.clientX;
+       stY=e.clientY;
+    }
+    const handleMouseup=(e:MouseEvent)=>{
+      clicked=false
+          let w = e.clientX - stX;
+          let h = e.clientY - stY;
+          let x = stX + w / 2;
+          let y = stY + h / 2;
+          let rx = Math.abs(w) / 2;
+          let ry = Math.abs(h) / 2;      
+      shapesRef.current.push({  type: "ellipse",
+                                  centerX: x,
+                                  centerY: y,
+                                  radiusX: rx,
+                                  radiusY: ry})
+                             
+    }
+    const handleMousemove=(e:MouseEvent)=>{
+      if(clicked){
+        renderShapes();
+        let w = e.clientX - stX;
+        let h = e.clientY - stY;
+        let x = stX + w / 2;
+        let y = stY + h / 2;
+        let rx = Math.abs(w) / 2;
+        let ry = Math.abs(h) / 2;
+        ctx.beginPath();
+        ctx.ellipse(x, y, rx, ry, 0, 0, 2 * Math.PI);
+        ctx.stroke();
+      }
+    }
+    canvas.addEventListener('mousedown',handleMousedown)
+    canvas.addEventListener('mouseup',handleMouseup)
+    canvas.addEventListener('mousemove',handleMousemove)
 
+      return()=>{  
+        canvas.removeEventListener("mousedown", handleMousedown);
+        canvas.removeEventListener("mouseup", handleMouseup);
+        canvas.removeEventListener("mousemove", handleMousemove);
+      }
+   }
+   
+
+  },[selected])
+  
+  const handleClear=()=>{
+    const canvas= canvasRef.current;
+    if(!canvas) return;
+    const ctx= canvas.getContext('2d');
+    if(ctx){
+       shapesRef.current=[]
+       ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    }
+  }
 
   return (
     <div className="w-screen h-screen bg-gray-300">
-         <canvas ref={canvasRef} className="h-full w-full bg-black">
+         <canvas ref={canvasRef} className="h-full w-full bg-[#121212] relative">
          </canvas>
+         <div className="absolute top-4 left-4 flex flex-col gap-1">
+            <button onClick={()=>{setSelected("rectangle")}} className="bg-white text-black">rectangle</button>
+            <button onClick={()=>{setSelected("ellipse")}} className="bg-white text-black">circle</button>
+            <button onClick={handleClear} className="bg-red-500 text-black">Clear</button>
+         </div>
+         
     </div>
   );
 }
