7ef161be8ba7c10ac05ab38f2d5461b58d281407
Rishit <rishupal631@gmail.com>
2025-09-07 16:51:22 +0000

    CORS fix

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -6,7 +6,11 @@ import chatRouter from './routes/chat.routes';
 import dotenv from 'dotenv'
 
 const app=express();
-app.use(cors());
+app.use(cors({
+  origin: "https://kalam-kaar-web.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json())
 dotenv.config()
 
